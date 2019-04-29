cb4a66a13e066f5b559b3e76bb80a5fee4ac0e12
Luis Jasso <luisnoejasso@gmail.com>
2019-04-29 14:37:48 -0500

    Updates

---
+++ precache-manifest.058c12bdbb030543fc0a034ab69be705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7b947f1a972f1f8eed3",
+    "url": "/shotwatcher/static/css/main.10937bd0.chunk.css"
+  },
+  {
+    "revision": "f7b947f1a972f1f8eed3",
+    "url": "/shotwatcher/static/js/main.f7b947f1.chunk.js"
+  },
+  {
+    "revision": "e8166ad6dece3a815566",
+    "url": "/shotwatcher/static/js/1.e8166ad6.chunk.js"
+  },
+  {
+    "revision": "6ce4bf170eb4388e56bd",
+    "url": "/shotwatcher/static/js/runtime~main.6ce4bf17.js"
+  },
+  {
+    "revision": "f32a8dfd6749d5b222873a4fb0c60990",
+    "url": "/shotwatcher/index.html"
+  }
+];
